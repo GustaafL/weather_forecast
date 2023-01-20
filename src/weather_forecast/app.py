50ccc4d50c1df68de9f8e41364b817942b4c9c02
GustaafL <41048720+GustaafL@users.noreply.github.com>
2023-01-20 09:10:12 +0100

    fix(app.py): remove unused endpoint

--- src/weather_forecast/app.py
+++ src/weather_forecast/app.py
@@ -104,14 +104,6 @@ def resource_not_found(e):
     return jsonify(error=str(e)), 404
 
 
-@app.route("/cheese")
-def get_one_cheese():
-    resource = None
-
-    if resource is None:
-        abort(404, description="Resource not found")
-
-    return jsonify(resource)
 
 
 if __name__ == "__main__":
